936b68626ad0d93010940d2341d1dcd76fe70eb0
Vansh Srivasta <vanshrivastav2002@gmail.com>
2024-05-11 07:25:44 +0530

    Education page edit

--- contactenquiry/views.py
+++ contactenquiry/views.py
@@ -3,7 +3,7 @@ from contactenquiry .models import contactEnquiry
 
 
 def contact(request):
-    return render(request,'portfolio/home.html')
+    return render(request,'portfolio/contact.html')
 
 def saveEnquiry(request):
     if request.method == "POST":
